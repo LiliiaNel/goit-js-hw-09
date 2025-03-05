2e3c8c69135654a7f6a4d81c0d34b7ccd9974580
Lili <Liliia_Nel@proton.me>
2025-03-05 17:53:34 +0100

    fix form js, add alert

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -11,33 +11,41 @@ form.addEventListener("input", onFormInput);
 
 function onFormInput(event) {
     if (event.target.nodeName === "INPUT") {
-   formData.email = event.target.value;
-  }
+        formData.email = event.target.value;
+    }
     if (event.target.nodeName === "TEXTAREA") {
-     formData.message = event.target.value;
+        formData.message = event.target.value;
     }    
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    console.log(formData);
+    // console.log(formData);
 }
 
 
 function dataFromLocalStorage() {
     const savedMsg = localStorage.getItem(STORAGE_KEY);
-      if (!savedMsg) {
-    return;
+    if (savedMsg === null || typeof savedMsg !== "string") {
+        return;
     }
-    const parsedMsg = JSON.parse(savedMsg);  
-     Object.keys(parsedMsg).forEach((key) => {
-     formData[key] = parsedMsg[key];
-    formData[key].value = parsedMsg[key].value;
-});
-    console.log(formData);
+    savedMsg.trim();
+    const parsedMsg = JSON.parse(savedMsg); 
+    console.log(parsedMsg);
+    Object.keys(parsedMsg).forEach((key) => {
+        formData[key] = parsedMsg[key];
+    });
+    
 }
 
 
 function onFormSubmit(event) {
     event.preventDefault();
-    localStorage.removeItem(STORAGE_KEY);
-    const targetForm = event.currentTarget;
-    targetForm.reset();
+    if (formData.email === "" || formData.message === "") {
+        alert("Fill please all fields");
+        return;
+    } else {
+        console.log(formData); 
+        localStorage.removeItem(STORAGE_KEY);
+        const targetForm = event.currentTarget;
+        targetForm.reset();
+    }
+   
 }
